86c6ae67ff0f4d8edeacf511c8c541a5c188cf5d
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-11 16:31:32 +0200

    calculate_distance and calculate_angle fucntions declared

--- physic.py
+++ physic.py
@@ -8,6 +8,12 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 700
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def calculate_distance(p1, p2):
+    return math.sqrt((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)
+
+def calculate_angle(p1, p2):
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+
 def draw(space, window, draw_options):
     window.fill("white")
     space.debug_draw(draw_options)
@@ -58,6 +64,10 @@ def run(window, width, height):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                ball.body.apply_impulse_at_local_point((10000, 0), (0, 0))
+
         draw(space, window, draw_options)
         space.step(dt)
         clock.tick(fps)
