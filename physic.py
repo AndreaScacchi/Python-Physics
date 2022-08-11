b2232a2c9d9255b98913587ae288aae04e2bba65
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-11 12:46:00 +0200

    run function declared, and inside it the while loop can run a maximum of 60 fps, regulating the speed

--- physic.py
+++ physic.py
@@ -7,3 +7,11 @@ pygame.init()
 
 WIDTH, HEIGHT = 1000, 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def run(window, width, height):
+    run = True
+    clock = pygame.time.Clock()
+    fps = 60
+
+    while run:
+        clock.tick(fps)
